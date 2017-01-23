c9aff678209f3798f70561c6027055558e520ea9
Aaron Czichon <aaron@Aarons-MacBook-Pro.local>
2017-01-23 15:51:55 +0100

    iOS Fix

--- src/ios/Version.m
+++ src/ios/Version.m
@@ -21,10 +21,10 @@
     NSString* appName = [[[NSBundle mainBundle]infoDictionary]objectForKey :@"CFBundleDisplayName"];
     // Build result object
     NSMutableDictionary* returnInfo = [NSMutableDictionary dictionaryWithCapacity:4];
-    [returnInfo setObject:[NSString:appName] forKey:@"appName"];
-    [returnInfo setObject:[NSString:packageName] forKey:@"packageName"];
-    [returnInfo setObject:[NSString:versionNumber] forKey:@"versionNumber"];
-    [returnInfo setObject:[NSString:versionCode] forKey:@"versionCode"];
+    [returnInfo setValue:[appName] forKey:@"appName"];
+    [returnInfo setValue:[packageName] forKey:@"packageName"];
+    [returnInfo setValue:[versionNumber] forKey:@"versionNumber"];
+    [returnInfo setValue:[versionCode] forKey:@"versionCode"];
     // Give it back to the Javascript
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:returnInfo];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
