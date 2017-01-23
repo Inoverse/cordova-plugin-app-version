7089bbee4c05eab407a608612ac4971154443572
Aaron Czichon <aaron@Aarons-MacBook-Pro.local>
2017-01-23 15:48:56 +0100

    iOS string fix

--- src/ios/Version.m
+++ src/ios/Version.m
@@ -21,10 +21,10 @@
     NSString* appName = [[[NSBundle mainBundle]infoDictionary]objectForKey :@"CFBundleDisplayName"];
     // Build result object
     NSMutableDictionary* returnInfo = [NSMutableDictionary dictionaryWithCapacity:4];
-    [returnInfo setObject:[NSString*:appName] forKey:@"appName"];
-    [returnInfo setObject:[NSString*:packageName] forKey:@"packageName"];
-    [returnInfo setObject:[NSString*:versionNumber] forKey:@"versionNumber"];
-    [returnInfo setObject:[NSString*:versionCode] forKey:@"versionCode"];
+    [returnInfo setObject:[messageAsString:appName] forKey:@"appName"];
+    [returnInfo setObject:[messageAsString:packageName] forKey:@"packageName"];
+    [returnInfo setObject:[messageAsString:versionNumber] forKey:@"versionNumber"];
+    [returnInfo setObject:[messageAsString:versionCode] forKey:@"versionCode"];
     // Give it back to the Javascript
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:returnInfo];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
